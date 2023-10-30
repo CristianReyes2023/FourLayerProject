6c7c42919b6ae3642d90a5e5ce318f61a2e2bc67
CristianReyes2023 <137292974+CristianReyes2023@users.noreply.github.com>
2023-10-30 12:23:14 -0500

    feat: :sparkles: JWT in progress

---
+++ Dominio/Entities/User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dominio.Entities;
+public class User
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public IColl MyProperty { get; set; }
+}
